94743395d748e7ca1558887120478fbfad576ac7
User <User@192.168.200.155>
2021-11-12 20:32:43 +0900

    multiDraw

--- gui/src/basic/Ex12_dragdraw.java
+++ gui/src/basic/Ex12_dragdraw.java
@@ -7,7 +7,9 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -38,7 +40,10 @@ class Nemo2 {
 
 class DrawPanel extends JPanel implements MouseListener, MouseMotionListener, KeyListener{
 	
+	private ArrayList<Nemo2> rectGroup = new ArrayList<Nemo2>();
 	private Nemo2 rect = new Nemo2();
+	private JButton reset = new JButton("reset");
+	
 	private int startX;
 	private int startY;
 	private int nowX;
@@ -53,11 +58,27 @@ class DrawPanel extends JPanel implements MouseListener, MouseMotionListener, Ke
 		addMouseListener(this);
 		addMouseMotionListener(this);
 		addKeyListener(this);
+		setResetButton();
+	}
+	
+	private void setResetButton() {
+		this.reset.setBounds(650,500,100,50);
+		this.reset.setBackground(Color.gray);
+		this.reset.addMouseListener(this);
+		add(this.reset);
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		
+		if(this.rectGroup.size() > 0) {
+			g.setColor(Color.blue);
+			for(int i=0; i<this.rectGroup.size(); i++) {
+				Nemo2 r = this.rectGroup.get(i);
+				g.drawRect(r.getX(), r.getY(), r.getW(), r.getH());
+			}
+		}
 		
 		if(this.isDrawing) g.setColor(Color.gray);
 		else g.setColor(Color.blue);
@@ -150,14 +171,20 @@ class DrawPanel extends JPanel implements MouseListener, MouseMotionListener, Ke
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		this.startX = e.getX();
-		this.startY = e.getY();
-		this.isDrawing = true;
+		if(e.getSource() == this.reset) {
+			resetDraw();
+		}
+		else {
+			this.startX = e.getX();
+			this.startY = e.getY();
+			this.isDrawing = true;
+		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		this.isDrawing = false;
+		this.rectGroup.add(this.rect);
 	}
 
 	@Override
@@ -174,6 +201,13 @@ class DrawPanel extends JPanel implements MouseListener, MouseMotionListener, Ke
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+	}
+	
+	private void resetDraw() {
+		this.rect = new Nemo2();
+		this.rectGroup.clear();
+		this.isDrawing = false;
+		this.shiftKey = false;
 	}
 }
 
